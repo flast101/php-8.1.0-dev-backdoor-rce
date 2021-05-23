d99a4c760c46d84c81e5d66d99a3db0d90a00ea2
flast101 <frederic.lastennet@gmail.com>
2021-05-23 14:34:00 +0200

    Update backdoor_php_8.1.0-dev.py

--- backdoor_php_8.1.0-dev.py
+++ backdoor_php_8.1.0-dev.py
@@ -1,18 +1,21 @@
 """
-Exploit Title: PHP 8.1.0-dev Backdoor Remote Command Execution
+Exploit Title: PHP 8.1.0-dev Backdoor Remote Code Execution
 Date: 23 may 2021
 Exploit Author: flast101
 Vendor Homepage: https://www.php.net/
 Software Link: 
-- https://hub.docker.com/r/phpdaily/php
-- https://github.com/phpdaily/php
+    - https://hub.docker.com/r/phpdaily/php
+    - https://github.com/phpdaily/php
 Version: 8.1.0-dev
-Tested on: 8.1.0-dev
+Tested on: Ubuntu 20.04
 CVE : N/A
 References:
-- https://github.com/php/php-src/commit/2b0f239b211c7544ebc7a4cd2c977a5b7a11ed8a
-- https://github.com/vulhub/vulhub/blob/master/php/8.1-backdoor/README.zh-cn.md
+    - https://github.com/php/php-src/commit/2b0f239b211c7544ebc7a4cd2c977a5b7a11ed8a
+    - https://github.com/vulhub/vulhub/blob/master/php/8.1-backdoor/README.zh-cn.md
 
+Blog: https://flast101.github.io/php-8.1.0-dev-backdoor-rce/
+Download: https://github.com/flast101/php-8.1.0-dev-backdoor-rce/blob/main/backdoor_php_8.1.0-dev.py
+Contact: flast101.sec@gmail.com
 
 An early release of PHP, the PHP 8.1.0-dev version was released with a backdoor on March 28th 2021, but the backdoor was quickly discovered and removed. If this version of PHP runs on a server, an attacker can execute arbitrary code by sending the User-Agentt header.
 The following exploit uses the backdoor to provide a pseudo shell ont the host.
